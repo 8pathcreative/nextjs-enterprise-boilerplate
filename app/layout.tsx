b54a1a7ffd639418a4dab1fbc0fcac891fe59865
8path Studio <neilhumphreydesign@gmail.com>
2025-02-21 21:21:28 +0000

    hh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "styles/tailwind.css"
+import React from 'react';
+import { JSX } from 'react/jsx-runtime';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
