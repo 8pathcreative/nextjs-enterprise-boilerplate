ce893361d6e0926c4fc1db6a8bff16f00b39fd74
8pathcreative <neilhumphreydesign@gmail.com>
2025-02-24 15:20:14 -0600

    x

---
+++ src/components/Button/ShadButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@shadcn/ui";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Enterprise App</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
